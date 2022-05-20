7063e812db3c8e7d833929ffc9728dc9395c09aa
Sajoniks <avesom33@mail.ru>
2022-05-20 21:12:53 +0300

    Bot server

---
+++ LearnBotServer/Program.cs
@@ -0,0 +1,21 @@
+using LearnBotServer.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpLogging();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
